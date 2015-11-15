2759deb904267274ea3317bcac2df1da80f0a99a
seaberg <jonas.sj@icloud.com>
2015-11-15 21:28:33 +0000

    Improved PriceHistoryFetcher.rb

--- main.rb
+++ main.rb
@@ -5,8 +5,6 @@ sf = StockFetcher.new
 
 stock_prices = sf.fetch_price("AAK.ST", "2015-10-01", "2015-10-31")
 
-# puts stock_prices
-
 phs = PriceHistoryStorage.new
 phs.store_price_history(stock_prices)
 phs.retrieve_price_history("AAK.ST")
